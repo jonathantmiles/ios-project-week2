aafcb0a03caaaed79da284957802aeb18effd63f
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-09-25 10:49:52 -0700

    includes an OAuth 3rd-party library and more cocoa touch classes

---
+++ Obsidian Portal App Interface/Cells/CharacterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CharacterTableViewCell.swift
+//  Obsidian Portal App Interface
+//
+//  Created by Jonathan T. Miles on 9/25/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Obsidian Portal App Interface/Cells/PagesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PagesTableViewCell.swift
+//  Obsidian Portal App Interface
+//
+//  Created by Jonathan T. Miles on 9/25/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class PagesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
